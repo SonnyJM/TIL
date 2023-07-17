75ef58dfcb980d9bdf320e612a295d1a7376c823
손종민 <kain9101@naver.com>
2023-07-17 11:00:34 +0900

    day1 1강 정리

---
+++ 01_python_week1/practice.py
@@ -0,0 +1,23 @@
+a = 1
+b = 2
+c = a + b
+
+num1 = 1
+num2 = 2
+sum_result = num1 + num2
+
+number = 25
+temperature = 25
+# 문제에 맞게 변수명 설정
+
+is_hot = temperature > 30 # is로 시작하면 True or False를 리턴하는 함수
+
+numbers = [1, 2, 3, 4, 5]
+
+for number in numbers: # 반복문에서 임시변수 사용
+
+
+# 시간 예시
+초 분 시
+# 상수 : 변하지 않는 숫자
+SECONDS_PER_MINUTE = 60
